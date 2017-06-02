3c5f62ba71b21686c2d2e80786f126b99aabcdad
hu00224083 <horak.balazs@spar.hu>
2017-06-02 14:01:14 +0200

    The mysql source of the project. (also deleting dummy test file: file22.xml)

---
+++ mysql/ulyssys_hw_ddl.sql
@@ -0,0 +1,63 @@
+CREATE SCHEMA `ulyssyshw` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE `ulyssyshw`.`user_type` (
+  `user_type_code` VARCHAR(20) NOT NULL,
+  `Description` VARCHAR(45) NULL,
+  PRIMARY KEY (`user_type_code`))
+COMMENT = 'Constraint for user type. (Enum in java)';
+
+CREATE TABLE `ulyssyshw`.`user` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) NOT NULL,
+  `login` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `type` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX `login_UNIQUE` (`login` ASC))
+COMMENT = 'Application users.';
+
+ALTER TABLE `ulyssyshw`.`user` 
+ADD INDEX `fk_user_type_idx` (`type` ASC);
+ALTER TABLE `ulyssyshw`.`user` 
+ADD CONSTRAINT `fk_user_type`
+  FOREIGN KEY (`type`)
+  REFERENCES `ulyssyshw`.`user_type` (`user_type_code`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+CREATE TABLE `ulyssyshw`.`application` (
+  `application_id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `amount` DOUBLE NOT NULL,
+  `approved` TIMESTAMP(3) NULL,
+  PRIMARY KEY (`application_id`),
+  INDEX `fk_user_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `ulyssyshw`.`user` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+COMMENT = 'Applications from the users.';
+
+CREATE TABLE `ulyssyshw`.`event` (
+  `event_id` INT NOT NULL AUTO_INCREMENT,
+  `tstamp` TIMESTAMP(3) NOT NULL,
+  `user_id` INT(11) NOT NULL,
+  `description` VARCHAR(200) NOT NULL,
+  `successful` VARCHAR(1) NOT NULL,
+  PRIMARY KEY (`event_id`),
+  INDEX `fk_user_id_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `ulyssyshw`.`user` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+COMMENT = 'User events.';
+
+ALTER TABLE `ulyssyshw`.`application` 
+ADD INDEX `idx_app_user_id` (`user_id` ASC);
+
+ALTER TABLE `ulyssyshw`.`application` 
+ADD INDEX `idx_app_approved` (`approved` ASC);
+
+
---
+++ mysql/ulyssys_hw_dml.sql
@@ -0,0 +1,11 @@
+INSERT INTO `ulyssyshw`.`user_type` (`user_type_code`, `Description`) VALUES ('applicant', 'Applicant requests applications.');
+INSERT INTO `ulyssyshw`.`user_type` (`user_type_code`, `Description`) VALUES ('operator', 'Operator makes decisions about applications.'); 
+
+INSERT INTO ulyssyshw.user (`user_id`,`name`,`login`,`password`,`type`) VALUES (1,'Test User 01','test01','f644a3bc48f18f88e1f94209c19cd9b1','applicant');
+INSERT INTO ulyssyshw.user (`user_id`,`name`,`login`,`password`,`type`) VALUES (2,'Test User 02','test02','e65fc37bc062a190681bc4fe07cb5fe4','applicant');
+INSERT INTO ulyssyshw.user (`user_id`,`name`,`login`,`password`,`type`) VALUES (3,'Test User 03','test03','b051796e4bc4e87b9eb81e24f4387e22','applicant');
+INSERT INTO ulyssyshw.user (`user_id`,`name`,`login`,`password`,`type`) VALUES (4,'Operator User01','operator01','01c25bbea26630e68d33d6b2f82c6ade','operator');
+INSERT INTO ulyssyshw.user (`user_id`,`name`,`login`,`password`,`type`) VALUES (5,'Operator User02','operator02','c0785660f4476aea3998588217c99e73','operator');
+INSERT INTO ulyssyshw.user (`user_id`,`name`,`login`,`password`,`type`) VALUES (6,'Operator User03','operator03','43b4cd3f5cfe229be7fb300fc3a07b64','operator');
+commit;
+
